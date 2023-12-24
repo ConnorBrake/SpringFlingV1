570d4588e077317ce08f13c4eabb8b2525a57c78
Connor Brake <153332456+ConnorBrake@users.noreply.github.com>
2023-12-24 13:34:37 -0500

    With Drag

--- src/main/java/com/mycompany/springflingv1/SpringFling.java
+++ src/main/java/com/mycompany/springflingv1/SpringFling.java
@@ -17,7 +17,10 @@ public class SpringFling {
         double horizontalAcceleration = 0;
         double verticalVelocity = 0;
         double verticalDistance = 0;
-        double verticalAccleration = 0;
+        double verticalAcceleration = 0;
+        //Forces of Air Resistance
+        double horizontalAirResistance = 0;
+        double verticalAirResistance = 0;
         //Gets Required Values 
         Scanner input = new Scanner(System.in); 
         System.out.println("Input The Launch Angle");
@@ -26,6 +29,10 @@ public class SpringFling {
         double distanceToBucket = input.nextDouble();
         System.out.println("Input The Spring Constant of Your Spring");
         double springConstant = input.nextDouble();
+        System.out.println("Input The Cross Sectional Area of Your Spring");
+        double springCrossSectionalArea = input.nextDouble();
+        System.out.println("Input The Mass of Your Spring");
+        double springMass = input.nextDouble();
         
         //Calulates The Springs Starting Height
         double rampHeight = Math.sin(Math.toRadians(launchAngle))*0.612;
@@ -39,17 +46,36 @@ public class SpringFling {
             launchSpeed += 0.01;
             //Resets The Components of The Launch Speed
             horizontalVelocity = Math.cos(Math.toRadians(launchAngle))*launchSpeed;
+            horizontalAirResistance = -1*0.5*0.85*1.23*springCrossSectionalArea*Math.pow(horizontalVelocity, 2);
             horizontalDistance = 0;
+            horizontalAcceleration = horizontalAirResistance/springMass;
+            
             verticalVelocity = Math.sin(Math.toRadians(launchAngle))*launchSpeed;
+            verticalAirResistance = -1*0.5*0.85*1.23*springCrossSectionalArea*Math.pow(verticalVelocity, 2);
             verticalDistance = springInitialHeight;
+            verticalAcceleration = (verticalAirResistance/springMass) - 9.807;
+            
             System.out.println(horizontalDistance + " " + verticalDistance);
+            
             for(double time = 0; time < 3.50; time += 0.01)
             {
-                horizontalDistance = horizontalDistance + (horizontalVelocity*(0.01));
-                horizontalVelocity = horizontalVelocity;
-                verticalDistance = verticalDistance + (verticalVelocity*(0.01)) + (0.5*-9.807*(Math.pow((0.01), 2)));
-                verticalVelocity = (-9.807*(0.01)) + verticalVelocity;
+                horizontalDistance = horizontalDistance + (horizontalVelocity*(0.01)) + (0.5*horizontalAcceleration*(Math.pow((0.01), 2)));
+                horizontalVelocity = horizontalVelocity + horizontalAcceleration * (0.01);
+                horizontalAirResistance = -1*0.5*0.85*1.23*springCrossSectionalArea*Math.pow(horizontalVelocity, 2);
+                horizontalAcceleration = horizontalAirResistance/springMass;
                 
+                verticalDistance = verticalDistance + (verticalVelocity*(0.01)) + (0.5*verticalAcceleration*(Math.pow((0.01), 2)));
+                verticalVelocity = (verticalAcceleration*(0.01)) + verticalVelocity;
+                //Changing Air Resistance Direction
+                if(((verticalDistance + (verticalVelocity*(0.01)) + (0.5*verticalAcceleration*(Math.pow((0.01), 2)))) - verticalDistance) > verticalDistance)
+                {
+                    verticalAirResistance = 1*0.5*0.85*1.23*springCrossSectionalArea*Math.pow(verticalVelocity, 2);
+                }
+                else
+                {
+                    verticalAirResistance = -1*0.5*0.85*1.23*springCrossSectionalArea*Math.pow(verticalVelocity, 2);
+                }
+                verticalAcceleration = (verticalAirResistance/springMass) - 9.807;
                 //For Testing Purposes
                 System.out.println(horizontalDistance + " " + verticalDistance);
                 //System.out.println(launchSpeed);
